01bc44d9cc683499d8edb247a578ba98eecccc4b
Will McGill <mcgill.will@yahoo.com>
2020-01-07 17:29:04 -0500

    added delete button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,7 +3,7 @@
   <div>
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <todo  v-for="todo in todos" :todo="todo"></todo>
+    <todo  @delete-todo="deleteTodo" v-for="todo in todos" :todo="todo"></todo>
   </div>
 </template>
 
@@ -16,5 +16,12 @@ export default {
   components: {
     Todo,
   },
+
+  methods:{
+    deleteTodo(todo) {
+      const todoIndex = this.todos.indexOf(todo);
+      this.todos.splice(todoIndex, 1);
+    }
+  }
 };
 </script>
